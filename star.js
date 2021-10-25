67961fc410c062fbf3a7252383916efea619dab8
LUH-3417 <92451342+LUH-3417@users.noreply.github.com>
2021-10-25 07:51:09 +0200

    Some changes

--- star.js
+++ star.js
@@ -1,28 +1,29 @@
 function setup() {
-    createCanvas(720, 400);
-    noStroke()
+    createCanvas(500, 500);
     colorMode(HSL)
+    fill (180,100,50)
+    noStroke()
   }
   
   function draw() {
-    background(300,100,100);
+    background(300,100,50);
   
     push();
-    translate(width * 0.2, height * 0.5);
-    rotate(frameCount / 200.0);
-    star(0, 0, 5, 70, 3);
+    translate(width * 0.2, height * 0.2);
+    rotate(frameCount / 100.0);
+    star(0, 0, 5, 70, 70);
     pop();
   
     push();
     translate(width * 0.5, height * 0.5);
     rotate(frameCount / 50.0);
-    star(0, 0, 80, 100, 40);
+    star(0, 0, 70, 150, 120);
     pop();
   
     push();
-    translate(width * 0.8, height * 0.5);
+    translate(width * 0.8, height * 0.8);
     rotate(frameCount / -100.0);
-    star(0, 0, 30, 70, 3);
+    star(0, 0, 30, 70, 70);
     pop();
   }
   
